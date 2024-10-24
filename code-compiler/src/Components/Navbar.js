1ddee182111d70a1d61dc2d2a2eda8cba58ade91
Siddiquei Wasia Fouzan <58743208+swasiafouzan@users.noreply.github.com>
2024-10-24 15:27:10 +0530

    Add files via upload

---
+++ code-compiler/src/Components/Navbar.js
@@ -0,0 +1,55 @@
+import React from "react";
+import Select from "react-select";
+import "./Navbar.css";
+
+const Navbar = ({
+  userLang,
+  setUserLang,
+  userTheme,
+  setUserTheme,
+  fontSize,
+  setFontSize,
+}) => {
+  const languages = [
+    { value: "C", label: "C" },
+    { value: "CPP", label: "C++" },
+    { value: "python", label: "Python" },
+    { value: "java", label: "Java" },
+  ];
+
+  const themes = [
+    { value: "vs-dark", label: "Dark" },
+    { value: "light", label: "Light" },
+  ];
+
+  return (
+    <div className="navbar">
+      <h1>Geeks Code Compiler</h1>
+      <Select
+        options={languages}
+        value={userLang}
+        onChange={(e) => setUserLang(e.value)}
+        placeholder={userLang}
+      />
+      <Select
+        options={themes}
+        value={userTheme}
+        onChange={(e) => setUserTheme(e.value)}
+        placeholder={userTheme}
+      />
+      <label>FOnt Size</label>
+      <input
+        type="range"
+        min="18"
+        max="30"
+        value={fontSize}
+        step="2"
+        onChange={(e) => {
+          setFontSize(e.target.value);
+        }}
+      />
+    </div>
+  );
+};
+
+export default Navbar;
